92332ee3883eddbebb298c74ff44f3d04d611a81
Lukas Thüring Jansen <luk.thuering@gmail.com>
2020-11-11 07:59:48 +0100

    init aufgabe 2

---
+++ Aufgabe A2-1-2/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Aufgabe_A2_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.CmdDelete = new System.Windows.Forms.Button();
+            this.CmdEnter = new System.Windows.Forms.Button();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.lblLastname = new System.Windows.Forms.Label();
+            this.lblFirstname = new System.Windows.Forms.Label();
+            this.NudAge = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.NudAge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(15, 114);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 13;
+            // 
+            // CmdDelete
+            // 
+            this.CmdDelete.Location = new System.Drawing.Point(122, 133);
+            this.CmdDelete.Name = "CmdDelete";
+            this.CmdDelete.Size = new System.Drawing.Size(75, 23);
+            this.CmdDelete.TabIndex = 12;
+            this.CmdDelete.Text = "Löschen";
+            this.CmdDelete.UseVisualStyleBackColor = true;
+            this.CmdDelete.Click += new System.EventHandler(this.CmdDelete_Click);
+            // 
+            // CmdEnter
+            // 
+            this.CmdEnter.Location = new System.Drawing.Point(18, 133);
+            this.CmdEnter.Name = "CmdEnter";
+            this.CmdEnter.Size = new System.Drawing.Size(75, 23);
+            this.CmdEnter.TabIndex = 11;
+            this.CmdEnter.Text = "Ausgeben";
+            this.CmdEnter.UseVisualStyleBackColor = true;
+            this.CmdEnter.Click += new System.EventHandler(this.CmdEnter_Click);
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(97, 52);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(100, 20);
+            this.txtLastname.TabIndex = 10;
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Location = new System.Drawing.Point(97, 19);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstname.TabIndex = 9;
+            // 
+            // lblLastname
+            // 
+            this.lblLastname.AutoSize = true;
+            this.lblLastname.Location = new System.Drawing.Point(12, 55);
+            this.lblLastname.Name = "lblLastname";
+            this.lblLastname.Size = new System.Drawing.Size(59, 13);
+            this.lblLastname.TabIndex = 8;
+            this.lblLastname.Text = "Nachname";
+            // 
+            // lblFirstname
+            // 
+            this.lblFirstname.AutoSize = true;
+            this.lblFirstname.Location = new System.Drawing.Point(12, 19);
+            this.lblFirstname.Name = "lblFirstname";
+            this.lblFirstname.Size = new System.Drawing.Size(49, 13);
+            this.lblFirstname.TabIndex = 7;
+            this.lblFirstname.Text = "Vorname";
+            // 
+            // NudAge
+            // 
+            this.NudAge.Location = new System.Drawing.Point(15, 78);
+            this.NudAge.Maximum = new decimal(new int[] {
+            115,
+            0,
+            0,
+            0});
+            this.NudAge.Name = "NudAge";
+            this.NudAge.Size = new System.Drawing.Size(179, 20);
+            this.NudAge.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NudAge);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.CmdDelete);
+            this.Controls.Add(this.CmdEnter);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.lblLastname);
+            this.Controls.Add(this.lblFirstname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.NudAge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button CmdDelete;
+        private System.Windows.Forms.Button CmdEnter;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.Label lblFirstname;
+        private System.Windows.Forms.NumericUpDown NudAge;
+    }
+}
+
---
+++ Aufgabe A2-1-2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aufgabe_A2_1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdEnter_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = txtFirstname.Text + "" + txtLastname.Text + "" + NudAge.Value;
+        }
+
+        private void CmdDelete_Click(object sender, EventArgs e)
+        {
+            txtFirstname.Text = "";
+            txtLastname.Text = "";
+            lblResult.Text = "";
+            NudAge.Value = 0;
+        }
+    }
+}
